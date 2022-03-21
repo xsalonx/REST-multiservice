41155d7bf6cb3fe1a8ab11224736465f80e579d3
xsalonx <lukasz.dubiel3@gmail.com>
2022-03-21 01:57:39 +0100

    a

--- index.js
+++ index.js
@@ -9,10 +9,33 @@ const dataProvider = new DataProvider();
 
 httpServer.addStaticPath("./public", "/")
 
-httpServer.get("/getAuthor", (req, res) => {
+httpServer.get("/get-data", async (req, res) => {
     const query = req.query;
+    const authors = await dataProvider.getData(
+        "https://reststop.randomhouse.com/resources/authors?",
+        {
+            lastName: query.lastName,
+            firstName: query.firstName},
+        [
+            "authorid",
+            "authorfirst",
+            "authorlast"],
+        "author"
+    );
+    console.log("authors")
+    console.log(authors)
+
+    const books = await dataProvider.getData(
+        "https://reststop.randomhouse.com/resources/titles?",
+        {search: query.keywords},
+        ["titleweb"],
+        "title"
+    )
+    console.log("books")
+    console.log(books)
+
+    res.json([authors, books].filter(v => v))
 
 
 })
 
-dataProvider.getAuthor("Grisham")
--- public/javascripts/Model.js
+++ public/javascripts/Model.js
@@ -1,14 +1,67 @@
 
-
 export default class Model {
     constructor() {
 
+        const pparser = (v) => v.split(" ").filter(v => v.length > 0).join("%20")
+        this.inputsConfig = {
+            "lastName": pparser,
+            "firstName": pparser,
+            "keywords": pparser
+        }
 
+        this.inputsNames = Object.entries(this.inputsConfig).map(([k, v]) => k)
+
+        const inputList = document.getElementById("input-forms-list")
+        inputList.innerHTML = this.genFormHTML()
     }
 
-    async fetchAuthorData(lastName) {
-        fetch('/api/getAuthor')
+    genFormHTML() {
+        return this.inputsNames
+            .map(name => `
+                        <label> ${name}
+                        <input type="text" placeholder="" id="input-${name}">
+                        </label>`)
+            .join("")
+    }
+
+    async fetchAuthorData() {
+        console.log("fetchAuthorData")
+        const params = this.inputsNames
+            .map(name => [
+                name,
+                this.inputsConfig[name](document.getElementById(`input-${name}`)?.value)
+            ])
+            .filter(([k,v]) => v)
+            .map(([k,v]) => `${k}=${v}`)
+            .join("&")
+
+        if (! params.length > 0) {
+            alert("some input needed")
+            return
+        }
+
+        const endpoint = "/api/get-data/?" + params
+        fetch(endpoint)
             .then(response => response.json())
-            .then(data => console.log(data));
+            .then(dataList => {
+                console.log(dataList)
+                this.createTables(dataList)
+
+            })
+            .catch(e => console.error(e))
+    }
+
+    createTables(dataList) {
+        document.getElementById("tables-div").innerHTML =
+            dataList.map(d => this.createTable(d)).join("")
+    }
+
+    createTable(dataObjs) {
+        const columns = Object.entries(dataObjs[0]).map(([k, v]) => k)
+        const intoTr = (s) => `<tr>${s}</tr>`
+        const tableHeader = intoTr(columns.map(n => `<th>${n}</th>`))
+        const rows = dataObjs.map(obj => intoTr(columns.map(n => `<th>${obj[n]}</th>`)))
+
+        return `<table>${tableHeader}${rows.join("")}</table>`
     }
 }
